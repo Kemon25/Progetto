6a8bf4ddb6f6e078cb16e18dd6b4526b68d16026
gabri <gabri@pc-gab.homenet.telecomitalia.it>
2022-11-15 12:53:38 +0100

    Junit Test DocenteDAO

---
+++ GestioneCorsi/src/main/java/it/betacom/architecture/dbaccess/DBAccessTest.java
@@ -0,0 +1,30 @@
+package it.betacom.architecture.dbaccess;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.Test;
+
+import it.betacom.architecture.dao.DAOException;
+
+class DBAccessTest {
+
+	@Test
+	void testConnection() {
+		try {
+			DBAccess.getConnection();
+		} catch (DAOException | ClassNotFoundException | IOException exc) {
+			exc.printStackTrace();
+			fail("errore nel tentativo di apertura"+exc.getMessage());
+		}finally {
+			try {
+				DBAccess.closeConnection();
+			}catch (DAOException exc){
+				exc.printStackTrace();
+				fail("errore nel tentativo di chiusura"+exc.getMessage());
+			}
+		}
+	}
+
+}
---
+++ GestioneCorsi/src/main/java/test/it/betacom/architecture/dao/DocenteDAOTest.java
@@ -0,0 +1,49 @@
+package test.it.betacom.architecture.dao;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+import java.sql.Connection;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import it.betacom.architecture.dao.DAOException;
+import it.betacom.architecture.dao.DocenteDAO;
+import it.betacom.architecture.dbaccess.DBAccess;
+import it.betacom.businesscomponent.model.Docente;
+
+class DocenteDAOTest {
+	private static Connection conn;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		try {
+			conn = DBAccess.getConnection();
+		} catch (ClassNotFoundException | IOException | DAOException exc) {
+			System.out.println("Motivo: " + exc.getMessage());
+			exc.printStackTrace();
+		}
+	}
+
+
+	@Test
+	void test() {
+		try {
+			Docente docente = new Docente();
+			docente = DocenteDAO.getFactory().getById(conn, 4);
+			System.out.println(docente);
+			assertNotNull(docente);
+			
+		} catch (DAOException exc) {
+			System.out.println("Motivo: " + exc.getMessage());
+			exc.printStackTrace();
+		}
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		DBAccess.closeConnection();
+	}
+}
