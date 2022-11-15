ce10c11006ed9ca91c4f263c57616c4076ffa3ef
gabri <gabri@pc-gab>
2022-11-15 21:00:46 +0100

    Validazione e junit secondo

--- GestioneCorsi/src/main/java/it/betacom/businesscomponent/validate/Validazione.java
+++ GestioneCorsi/src/main/java/it/betacom/businesscomponent/validate/Validazione.java
@@ -1,6 +1,9 @@
 package it.betacom.businesscomponent.validate;
 
 import java.io.IOException;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 import it.betacom.architecture.dao.DAOException;
 import it.betacom.businesscomponent.CorsoCorsistaBC;
@@ -27,27 +30,46 @@ public class Validazione {
 	}
 
 	private boolean vStringa(String nome) {
-		String expression = "^[a-zA-Z ]+"; 
-		if(nome.length() <= 30) {
+		String expression = "^[a-zA-Z ]+";
+		if ((nome != null) && (nome.length() <= 30)) {
 			if (nome.matches(expression)) {
-				System.out.println("Testato");
 				return true;
 			}
 		}
 		return false;
 	}
-	
+
 	public boolean nomeCorsista(String nome) {
 		return vStringa(nome);
 	}
-	
+
 	public boolean cognomeCorsista(String cognome) {
 		return vStringa(cognome);
 	}
-	
+
 	public boolean nomeCorso(String nome) {
 		return vStringa(nome);
 	}
 
-	
+	public boolean dateCorso(Date inizio, Date fine) {
+		if (inizio != null && fine != null) {
+			GregorianCalendar calInizio = new GregorianCalendar();
+			GregorianCalendar calFine = new GregorianCalendar();
+			calInizio.setTime(inizio);
+			calFine.setTime(fine);
+
+			int anno = calFine.get(Calendar.YEAR) - calInizio.get(Calendar.YEAR);
+			int giorni = calFine.get(Calendar.DAY_OF_YEAR) - calInizio.get(Calendar.DAY_OF_YEAR);
+			if (anno != 0 || giorni >= 2)
+				return true;
+		}
+		return false;
+	}
+
+	public boolean commenti(String commento) {
+		if (commento.length() <= 200) {
+			return true;
+		}
+		return false;
+	}
 }
--- GestioneCorsi/src/main/java/test/it/betacom/businesscomponent/validate/ValidazioneTest.java
+++ GestioneCorsi/src/main/java/test/it/betacom/businesscomponent/validate/ValidazioneTest.java
@@ -1,9 +1,14 @@
 package test.it.betacom.businesscomponent.validate;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 
 import it.betacom.businesscomponent.validate.Validazione;
@@ -14,30 +19,61 @@ class ValidazioneTest {
 	private static String nome1;
 	private static String nome2;
 	private static String nome3;
+
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		validazione = Validazione.getFactory();
+
 		nome = "Angelo  ";
 		nome1 = "Ange5lo";
 		nome2 = "Angel&o";
 		nome3 = "AngeloAngeloAngeloAngeloAngeloaltro";
 	}
 
-
 	@Test
-	void test() {
+	@Order(1)
+	void vString() {
 		try {
-			validazione = Validazione.getFactory();
-			System.out.println(validazione.nomeCorsista(nome));
-			System.out.println(validazione.nomeCorsista(nome1));
-			System.out.println(validazione.nomeCorsista(nome2));
-			System.out.println(validazione.nomeCorsista(nome3));
+			assertEquals(true, validazione.nomeCorsista(nome));
+			assertEquals(false, validazione.nomeCorsista(nome1));
+			assertEquals(false, validazione.nomeCorsista(nome2));
+			assertEquals(false, validazione.nomeCorsista(nome3));
+			assertEquals(false, validazione.nomeCorsista(null));
 		} catch (Exception exc) {
 			fail("Motivo: " + exc.getMessage());
 		}
 	}
 
+	@Test
+	@Order(2)
+	void diffDateCorso() {
+		GregorianCalendar calInizio = new GregorianCalendar();
+		calInizio.set(Calendar.YEAR, 2000);
+		calInizio.set(Calendar.MONTH, 11);
+		calInizio.set(Calendar.DAY_OF_MONTH, 13);
+
+		GregorianCalendar calFine = new GregorianCalendar();
+		calFine.set(Calendar.YEAR, 2000);
+		calFine.set(Calendar.MONTH, 11);
+		calFine.set(Calendar.DAY_OF_MONTH, 13);
+		assertEquals(false, validazione.dateCorso(calInizio.getTime(), calFine.getTime()));
+
+		calFine.set(Calendar.YEAR, 2000);
+		calFine.set(Calendar.MONTH, 11);
+		calFine.set(Calendar.DAY_OF_MONTH, 17);
+		assertEquals(true, validazione.dateCorso(calInizio.getTime(), calFine.getTime()));
+
+		calFine.set(Calendar.YEAR, 2003);
+		calFine.set(Calendar.MONTH, 11);
+		calFine.set(Calendar.DAY_OF_MONTH, 13);
+		assertEquals(true, validazione.dateCorso(calInizio.getTime(), calFine.getTime()));
+
+		assertEquals(false, validazione.dateCorso(null, calFine.getTime()));
+
+	}
+
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
-		
+
 	}
 }
