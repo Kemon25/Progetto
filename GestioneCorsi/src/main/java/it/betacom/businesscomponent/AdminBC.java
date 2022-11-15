c906146ed8283735ca66096364d9101e554dd3ce
Betacom <Betacom@192.168.1.118>
2022-11-15 12:00:40 +0100

    CorsoCorsistaDAO

--- GestioneCorsi/src/main/java/it/betacom/businesscomponent/AdminBC.java
+++ GestioneCorsi/src/main/java/it/betacom/businesscomponent/AdminBC.java
@@ -1,12 +1,10 @@
 package it.betacom.businesscomponent;
 
-import it.betacom.businesscomponent.model.Admin;
+
 
 public class AdminBC {
 	
-	public boolean accesso(Admin admin) {
-		
-	}
+	
 	
 	
 	
