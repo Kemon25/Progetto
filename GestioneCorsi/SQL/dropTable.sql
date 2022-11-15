62c3ef54c0b38f9fbb87869a8c49621ffdf30c2e
Betacom <Betacom@DESKTOP-9B6KIAD.homenet.telecomitalia.it>
2022-11-15 10:47:09 +0100

    Add SQL files: - createTable.sql - dropTable.sql

---
+++ GestioneCorsi/SQL/dropTable.sql
@@ -0,0 +1,5 @@
+drop table admin cascade constraints;
+drop table corsista cascade constraints; 
+drop table corso cascade constraints;
+drop table docente cascade constraints;
+drop table corso_corsista cascade constraints;
