a49c610047e7d3e555f99cf6e9b2ce33cde76e9e
Betacom <Betacom@DESKTOP-T5CJ6UB.homenet.telecomitalia.it>
2022-11-16 11:21:09 +0100

    ste98fox: CorsistaBC eccezioni gestite

--- GestioneCorsi/src/main/java/it/betacom/businesscomponent/CorsistaBC.java
+++ GestioneCorsi/src/main/java/it/betacom/businesscomponent/CorsistaBC.java
@@ -14,43 +14,72 @@ import it.betacom.businesscomponent.validate.Validazione;
 public class CorsistaBC {
 	private Connection conn;
 	
-	public CorsistaBC() throws ClassNotFoundException, DAOException, IOException {
-		conn = DBAccess.getConnection();
+	public CorsistaBC() {
+		try {
+			conn = DBAccess.getConnection();
+		} catch(ClassNotFoundException | DAOException | IOException exc) {
+			exc.printStackTrace();
+			System.err.println(exc.getMessage());
+		}
 	}
 	
-	public boolean create(Corsista corsista) throws DAOException, ClassNotFoundException, IOException {
+	public boolean create(Corsista corsista) {
 		boolean valido = false;
 		
-		corsista.setId(CorsistaIdGenerator.getInstance().getNextId());
+		try {
+			corsista.setId(CorsistaIdGenerator.getInstance().getNextId());
 		
-		if((Validazione.getFactory().nomeCorsista(corsista.getNome())) &&
-				(Validazione.getFactory().cognomeCorsista(corsista.getCognome()))) {
-				//&& (Validazione.getFactory().precedentiFormativi(corsista.getPrecedentiFormativi())))
-			valido = true;
-			System.out.println("Nome e cognome nel formato corretto");
-		} else {
-			System.out.println("Nome o cognome non nel formato corretto");
-		}
-		
-		if(valido) {
-			CorsistaDAO.getFactory().create(conn, corsista);
-			System.out.println("Corsista creato!");
-		} else {
-			System.out.println("Corsista non creato!");
+			if((Validazione.getFactory().nomeCorsista(corsista.getNome())) &&
+					(Validazione.getFactory().cognomeCorsista(corsista.getCognome()))) {
+					//&& (Validazione.getFactory().precedentiFormativi(corsista.getPrecedentiFormativi())))
+				valido = true;
+				System.out.println("Nome e cognome nel formato corretto");
+			} else {
+				System.out.println("Nome o cognome non nel formato corretto");
+			}
+			
+			if(valido) {
+				CorsistaDAO.getFactory().create(conn, corsista);
+				System.out.println("Corsista creato!");
+			} else {
+				System.out.println("Corsista non creato!");
+			}
+		} catch(ClassNotFoundException | DAOException | IOException exc) {
+			exc.printStackTrace();
+			System.err.println(exc.getMessage());
 		}
 		
 		return valido;
 	}
 	
-	public ArrayList<Corsista> getAll() throws DAOException{
-		return CorsistaDAO.getFactory().getAll(conn);
+	public ArrayList<Corsista> getAll() {
+		ArrayList<Corsista> corsisti = new ArrayList<Corsista>();
+		try {
+			corsisti = CorsistaDAO.getFactory().getAll(conn);
+		} catch(DAOException exc) {
+			exc.printStackTrace();
+			System.err.println(exc.getMessage());
+		}
+		return corsisti;
 	}
 	
-	public Corsista getById(long id) throws DAOException {
-		return CorsistaDAO.getFactory().getById(conn, id);
+	public Corsista getById(long id) {
+		Corsista c = new Corsista();
+		try {
+			c = CorsistaDAO.getFactory().getById(conn, id);
+		} catch(DAOException exc) {
+			exc.printStackTrace();
+			System.err.println(exc.getMessage());
+		}
+		return c;
 	}
 	
-	public void delete(long id) throws DAOException {
-		CorsistaDAO.getFactory().delete(conn, id);
+	public void delete(long id) {
+		try {
+			CorsistaDAO.getFactory().delete(conn, id);
+		} catch(DAOException exc) {
+			exc.printStackTrace();
+			System.err.println(exc.getMessage());
+		}
 	}
 }
