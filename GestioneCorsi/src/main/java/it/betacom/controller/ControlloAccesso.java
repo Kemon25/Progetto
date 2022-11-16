bca1709a7d88211eb7caaddd687b2f5498e8b271
Betacom <Betacom@DESKTOP-6RP9REQ.homenet.telecomitalia.it>
2022-11-16 16:43:19 +0100

    Creazione ControlloAccesso

--- GestioneCorsi/src/main/java/it/betacom/controller/ControlloAccesso.java
+++ GestioneCorsi/src/main/java/it/betacom/controller/ControlloAccesso.java
@@ -1,41 +1,41 @@
 package it.betacom.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-/**
- * Servlet implementation class ControlloAccesso
- */
-@WebServlet("/ControlloAccesso")
+import it.betacom.businesscomponent.AdminBC;
+
+@WebServlet("/controlloAccesso")
 public class ControlloAccesso extends HttpServlet {
-	
+
 	private static final long serialVersionUID = -6441545525722074841L;
+	private int hitCount=1;
 
-	/**
-     * Default constructor. 
-     */
-    public ControlloAccesso() {
-        // TODO Auto-generated constructor stub
-    }
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		HttpSession session = request.getSession();
+		
+		session.setAttribute("hitCount", Integer.valueOf(hitCount));
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+		AdminBC aBC = new AdminBC();
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		if (aBC.accesso(username, password)) {
+			response.sendRedirect("home.jsp");
+			session.setAttribute("username", username);
+			session.setAttribute("password", password);
+		}
+		else 
+			response.sendRedirect("index.jsp");
+		
 	}
 
 }
