9094d06be834c7932c23f526d047752bc0169758
Betacom <Betacom@DESKTOP-6RP9REQ.homenet.telecomitalia.it>
2022-11-17 13:53:39 +0100

    immagine e modifica autodistruzione

--- GestioneCorsi/src/main/java/it/betacom/controller/Autodistruzione.java
+++ GestioneCorsi/src/main/java/it/betacom/controller/Autodistruzione.java
@@ -12,37 +12,54 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/autodistruzione")
 public class Autodistruzione extends HttpServlet {
 	private static final long serialVersionUID = -154792031843008944L;
-	private int contoRovescia= 3;
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	private int contoRovescia = 3;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
-		
+
 		out.println("<!DOCTYPE html>");
 		out.println("<html>");
 		out.println("<head><title>Errore login</title></head>");
 		out.println("<link rel=\"stylesheet\" href=\"css/style.css\">");
-		if(contoRovescia > 0) {
+		if (contoRovescia > 0) {
 			out.println("<meta http-equiv=\"refresh\" content=\"1; url=/GestioneCorsi/autodistruzione\" >");
-		
+
 			out.println("<body>");
-			
+
 			out.println("<div class=\"container\" style=\"text-align: center\">");
 			out.println("<header class=\"page.header\">");
 			out.println("<h1>Mi spiace, il computer si autodistruggerà in " + contoRovescia + " secondi</h1>");
 			contoRovescia--;
 			out.println("</header>");
 			out.println("</div>");
-			
+
 			out.println("</body>");
 			out.println("</html>");
-		}
-		else {
+			
+		} else {
+
 			out.println("<style>");
+			out.println("body, html {");
+			out.println("height: 100%;");
+			out.println("width: 100%;");
+			out.println("}");
 			out.println("body {");
-			out.println("background-color: black");
+			out.println("background-image: url('img/explosion.gif');");
+			out.println("background-position: center;");
+			out.println("background-repeat: no-repeat;");
+			out.println("background-size: cover;");
 			out.println("}");
 			out.println("</style>");
+			out.println("<script>");
+			out.println("setTimeout(function() {");
+			out.println(" window.close()");
+			out.println(" }, 1700);");
+			out.println("</script>");
+			out.println("</html>");
+			
+			contoRovescia=3;
 		}
 
 		out.close();
